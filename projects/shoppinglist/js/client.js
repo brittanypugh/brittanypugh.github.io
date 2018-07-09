c8d0752f84ae820ef96e9b89906a8f668b1be905
Brittany Pugh <pughbritt00@gmail.com>
2018-07-09 17:58:50 -0500

    add item counter
    add a counter to the bottom of the shopping list to keep track of the items added

--- projects/shoppinglist/js/client.js
+++ projects/shoppinglist/js/client.js
@@ -7,25 +7,41 @@ $('#add-item').on('click', function(event) {
 	event.preventDefault();
 	//get user input
 	var listItem = $('#item-input').val();
-	
 	if($.trim(listItem).length){ //if field is not empty
 		
-		//build list item html with checkbox and clear option
+		// Build list item html with checkbox and clear option
 		var itemHtml = "<li class='collection-item'><i class='material-icons item-check'>check_box_outline_blank</i><span class='item-text'>" + listItem + "</span><i class='material-icons item-remove'>clear</i></li>";
 
-		// Add the item to the shopping list
-		$('.shopping-list').append(itemHtml);
+		
+		var itemCount = $('.shopping-list li').length;
+		console.log("item count is: "+itemCount);
+		if(itemCount == 0){ //no items in the list
+			$('.shopping-list').append(itemHtml); // Add the item to the shopping list
+			$('.shopping-list').append("<li class='collection-item active'>Total Items<span class='new badge' data-badge-caption='Grocery Item'>1</span></li>"); // Add counter
+		}else{
+			$('.shopping-list li:last-child').remove(); // Remove counter
+			$('.shopping-list').append(itemHtml); // Add the item to the shopping list
+			$('.shopping-list').append("<li class='collection-item active'>Total Items<span class='new badge' data-badge-caption='Grocery Items'>" + itemCount + "</span></li>"); // Add counter with updated item count
+		}
 		
 		// Clear the text entered by the user
 		$('#item-input').val('');
-	}else{ //add warning
+	}else{ //Add warning
 		
 	}
 });
 	
 //Executed when user clicks the "X" on an added item
 $('.shopping-list').on('click', '.item-remove', function(event){
-	$(event.currentTarget).parent().remove();
+	$(event.currentTarget).parent().remove(); // Remove selected item
+	var itemCount = $('.shopping-list li').length - 1;
+	$('.shopping-list li:last-child').remove(); // Remove counter
+	if(itemCount == 1){
+		$('.shopping-list').append("<li class='collection-item active'>Total Items<span class='new badge' data-badge-caption='Grocery Item'>1</span></li>"); // Add counter
+	}else{
+		$('.shopping-list').append("<li class='collection-item active'>Total Items<span class='new badge' data-badge-caption='Grocery Items'>" + itemCount + "</span></li>"); // Add counter with updated item count
+	}
+	
 });
 	
 //Executed when user clicks the checkbox on an added item
@@ -38,6 +54,5 @@ $('.shopping-list').on('click', '.item-check', function(event){
         icon.innerHTML = "check_box_outline_blank";
     }
 });
-
 	
 });
