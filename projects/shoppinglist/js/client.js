8a2113e8aa1c8d1c1bc229b1f5ceed1757ad042f
Brittany Pugh <pughbritt00@gmail.com>
2018-07-10 19:59:53 -0500

    fix counter problem
    fix html for counter so item count is displayed for items other than 1, use local variables and add comments to make code readable

--- projects/shoppinglist/js/client.js
+++ projects/shoppinglist/js/client.js
@@ -4,28 +4,29 @@ $(document).ready(function(){
 	
 // Executed when user clicks "Add Item" button
 $('#add-item').on('click', function(event) {
+	
 	event.preventDefault();
-	//get user input
-	var listItem = $('#item-input').val();
-	if($.trim(listItem).length){ //if field is not empty
+	var listItem = $('#item-input').val(); //Get user input
+	var itemCount = $('.shopping-list li').length; //Get number of items
+	var itemHtml, counterHtml;
+	
+	if($.trim(listItem).length){ //If field is not empty
 		
-		// Build list item html with checkbox and clear option
-		var itemHtml = "<li class='collection-item'><i class='material-icons item-check'>check_box_outline_blank</i><span class='item-text'>" + listItem + "</span><i class='material-icons item-remove'>clear</i></li>";
-
+		itemHtml = "<li class='collection-item'><i class='material-icons item-check'>check_box_outline_blank</i><span class='item-text'>" + listItem + "</span><i class='material-icons item-remove'>clear</i></li>"; // Build list item html with checkbox and clear option
 		
-		var itemCount = $('.shopping-list li').length;
-		console.log("item count is: "+itemCount);
-		if(itemCount == 0){ //no items in the list
+		if(itemCount == 0){ //No items in the list
 			$('.shopping-list').append(itemHtml); // Add the item to the shopping list
-			$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='new badge' data-badge-caption='Grocery Item'>1</span></li>"); // Add counter
-		}else{
+			counterHtml = "<li class='collection-item active counter'>Total Items<span class='badge'>1</span></li>";
+			$('.shopping-list').append(counterHtml); // Add counter
+			
+		}else{ //At least one item in list
 			$('.shopping-list li:last-child').remove(); // Remove counter
 			$('.shopping-list').append(itemHtml); // Add the item to the shopping list
-			$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='new badge' data-badge-caption='Grocery Items'>" + itemCount + "</span></li>"); // Add counter with updated item count
+			counterHtml = "<li class='collection-item active counter'>Total Items<span class='badge'>" + itemCount + "</span></li>";
+			$('.shopping-list').append(counterHtml); // Add counter with updated item count
 		}
 		
-		// Clear the text entered by the user
-		$('#item-input').val('');
+		$('#item-input').val(''); // Clear the text entered by the user
 	}else{ //Add warning
 		
 	}
@@ -34,12 +35,13 @@ $('#add-item').on('click', function(event) {
 //Executed when user clicks the "X" on an added item
 $('.shopping-list').on('click', '.item-remove', function(event){
 	$(event.currentTarget).parent().remove(); // Remove selected item
-	var itemCount = $('.shopping-list li').length - 1;
 	$('.shopping-list li:last-child').remove(); // Remove counter
+	var itemCount = $('.shopping-list li').length; //Get number of items
+	
 	if(itemCount == 1){
-		$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='new badge' data-badge-caption='Grocery Item'>1</span></li>"); // Add counter
+		$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='badge'>1</span></li>"); // Add counter
 	}else{
-		$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='new badge' data-badge-caption='Grocery Items'>" + itemCount + "</span></li>"); // Add counter with updated item count
+		$('.shopping-list').append("<li class='collection-item active counter'>Total Items<span class='badge'>" + itemCount + "</span></li>"); // Add counter with updated item count
 	}
 	
 });
@@ -47,7 +49,9 @@ $('.shopping-list').on('click', '.item-remove', function(event){
 //Executed when user clicks the checkbox on an added item
 //Toggle between unchecked and checked box
 $('.shopping-list').on('click', '.item-check', function(event){ 
+	
 	var icon = event.currentTarget;
+	
 	if (icon.innerHTML === "check_box_outline_blank") {
         icon.innerHTML = "check_box";
     }else{
