9c1d54ee675b366494e42c2adb9700f63f704760
Brittany Pugh <pughbritt00@gmail.com>
2018-08-09 23:37:17 -0500

    fix quotation marks for cardHtml variable
    correct code for declaring and using variables

--- projects/nytarticles/js/client.js
+++ projects/nytarticles/js/client.js
@@ -1,14 +1,12 @@
-$(document).ready(function() 
-	var myKey = config.apiKey;
-    getData();
-); 
-			  
+var myKey = config.apiKey;
+getData();
+
 //create request and retrieve data
 function getData(){
 	//grab most shared articles in Magazine category from last 30 days
-	var url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/Magazine/30.json";
+	var url = "https://api.nytimes.com/svc/mostpopular/v2/mostshared/Opinion/30.json";
 	url += '?' + $.param({
-	  'api-key': myKey;
+	  'api-key': myKey
 	});
 	
 	$.ajax({
@@ -45,7 +43,9 @@ function appendErrorMessage(){
 
 //organize data with cards and add cards to page
 function appendData(result){
-	var url, author, title, abstract, published_date, rankTotalShares, thumbnail;
+	var url, author, title, abstract, publishedDate, rankTotalShares;
+	var thumbnail, thumbnailHeight, thumbnailWidth;
+	var cardHtml;
 	
 	url = result.url;
 	author = result.byline;
@@ -59,7 +59,8 @@ function appendData(result){
 	thumbnailHeight = result.media[0].media-metadata[0].height;
 	thumbnailWidth = result.media[0].media-metadata[0].width;
 	
-	cardHtml =  "<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><img src=thumbnail><span class="card-title">" + title + "</span></div><div class="card-content"><p>" + publishedDate + "</p><p>" + abstract + "</p></div><div class="card-action"><a href=url>View Article</a></div></div></div></div>";
+	cardHtml =  "<div class='row'><div class='col s12 m7'><div class='card'><div class='card-image'><img src=" + thumbnail + "><span class='card-title'>" + title + "</span></div><div class='card-content'><p>" + author + "</p><p>" + publishedDate + "</p><p>" + abstract + "</p></div><div class='card-action'><a href=" + url + ">View Article</a></div></div></div></div>";
+	
 	$("#cards").append(cardHtml);
 	
 }
